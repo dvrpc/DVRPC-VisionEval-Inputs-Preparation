02c8d2fbfb92c4ff1a6915a952ec079f6b600ba3
aaronfraint <aaronfraint@icloud.com>
2021-07-13 16:47:18 -0400

    revise package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="network_routing",
+    name="src",
     packages=find_packages(),
     version="0.0.1",
     description="ETL tools to prepare input data for the VisionEval model",
