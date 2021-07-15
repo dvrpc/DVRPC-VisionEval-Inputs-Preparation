9d497ed888bae29b5dfd12fef83e0df7edf1a3a8
aaronfraint <aaronfraint@icloud.com>
2021-07-15 14:52:00 -0400

    import data from DVRPC server

--- src/create_database.py
+++ src/create_database.py
@@ -1,6 +1,15 @@
 from src.environment_variables import DATABASE_URL
 from src.database import Database
+from src.get_data_from_DVRPC_portal import import_data_from_DVRPC_portal
 
+
+# You can list out any options available here: https://arcgis.dvrpc.org/portal/rest/services
+# Take care to match spelling and capitalization from the webpage!
+
+DVRPC_DATA_TO_COPY = [
+    ("Boundaries", ["MunicipalBoundaries"]),
+    ("Demographics", ["IPD_2018"]),
+]
 
 if __name__ == "__main__":
 
@@ -12,5 +21,8 @@ if __name__ == "__main__":
 
     # Make sure the PostGIS extension is loaded in the new database
     db.execute(f"CREATE EXTENSION IF NOT EXISTS postgis")
+
+    # Import data from DVRPC's ArcGIS Portal
+    import_data_from_DVRPC_portal(db, download_list=DVRPC_DATA_TO_COPY)
 
     print("Done")
