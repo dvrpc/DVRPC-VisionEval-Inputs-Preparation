0e4e5ecf5ec1bd0834e21cda545e8c11a0e137a0
aaronfraint <aaronfraint@icloud.com>
2021-07-15 14:35:27 -0400

    create db if it does not exist yet

--- src/create_database.py
+++ src/create_database.py
@@ -6,11 +6,9 @@ if __name__ == "__main__":
 
     db = Database()
 
-    # Get the database name from the DATABASE_URL variable
-    db_name = DATABASE_URL.split(r"/")[-1]
-
-    # TODO: check if database exists first
-    db.execute(f"CREATE DATABASE {db_name};", autocommit=True)
+    # Create the database if it doesn't exist yet
+    if not db.exists():
+        db.execute(f"CREATE DATABASE {db.db_name};", autocommit=True)
 
     # Make sure the PostGIS extension is loaded in the new database
     db.execute(f"CREATE EXTENSION IF NOT EXISTS postgis")
