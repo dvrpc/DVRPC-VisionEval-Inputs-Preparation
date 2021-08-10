89722ec97decbb71c59fddbb497a8bda45d9f009
Fiza Akram <fakram@dvrpc.org>
2021-08-10 19:59:32 -0400

    upload h5 into psql

--- src/urbansimh5.py
+++ src/urbansimh5.py
@@ -1,15 +1,21 @@
+from src.environment_variables import DATABASE_URL
 import pandas as pd
+from pandas.core.frame import DataFrame
 from database import Database
-data = pd.HDFStore('/Users/fakram/Downloads/results_dvrpc_run_335_run_results.h5',mode = 'r')
 
-type(data)
+h5data = pd.HDFStore('/Users/fakram/Downloads/results_dvrpc_run_335_run_results.h5',mode = 'r')
+
 #load dataframe into postgreSQL using Database.py 
 
 
+# 
+
 db = Database()
-#db.import...
-for tablename in data.keys():
-    df = data.get(tablename)
-    # generate tablename that doesnt have slashes or start with Num
-    
-    # db.import...()
+for tablename in h5data.keys():
+    # generate columnname that doesnt have slashes or start with Num
+    new_tablename = tablename.replace("/", "_")
+    new_tablename = "usim" + new_tablename
+    db.import_dataframe(h5data.get(tablename), new_tablename)
+
+
+
