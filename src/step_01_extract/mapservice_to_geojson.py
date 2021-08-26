7deece8a162a83e0f5abbe129054f853bec2a03a
Fiza Akram <fakram@dvrpc.org>
2021-08-26 16:06:24 -0400

    cleaned up extract files

--- src/step_01_extract/mapservice_to_geojson.py
+++ src/step_01_extract/mapservice_to_geojson.py
@@ -1,8 +1,14 @@
 import json
 import urllib.request as urlr
 
+from geopandas.geodataframe import GeoDataFrame
+from src.helpers.database import Database
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import shapely 
+from src.helpers.environment_variables import DATABASE_URL, SUPERUSER_DATABASE_URL
 
-def download_epa_sld_data_as_geojson():
+def download_epa_sld_data_as_geojson(filepath:str):
     """
     - This function downloads a geojson from the EPA's Smart Location Database
     - It uses a loop to iterate over all features in the table, which gets around
@@ -12,10 +18,10 @@ def download_epa_sld_data_as_geojson():
     # set variables for map service
     # map service url
     mapservice = (
-        r"https://geodata.epa.gov/arcgis/rest/services/OA/SmartLocationDatabase/MapServer/10/"
+        r"https://geodata.epa.gov/arcgis/rest/services/OA/SmartLocationDatabase/MapServer/15/"
     )
     # map service query
-    query = r"query?where=GEOID10+like+%2742%25%27"
+    query = r'query?where=GEOID10+like+%2734005%25%27+or+GEOID10+like+%2734007%25%27+or+GEOID10+like+%2734015%25%27+or+GEOID10+like+%2734021%25%27'
 
     # geojson structure
     dict = '{"type": "FeatureCollection"}'
@@ -30,9 +36,14 @@ def download_epa_sld_data_as_geojson():
 
     # query all objectids, count total objectids
     oidurl = mapservice + query + r"&returnIdsOnly=true&f=json"
+    print(oidurl)
+
     response = urlr.urlopen(oidurl)
+    print(response)
     oidjson = json.load(response)
+    print(oidjson) 
     idfield = oidjson["objectIdFieldName"]
+    print(idfield)
     idlist = oidjson["objectIds"]
     idlist.sort()
     numrec = len(idlist)
@@ -57,5 +68,24 @@ def download_epa_sld_data_as_geojson():
             geojson["features"].append(feature)
 
     # write final geojson to location on hard drive
-    with open(r"d:\temp\test.geojson", "w") as outfile:
+    file_path = r"/Users/fakram/Downloads/SLD.geojson"
+
+    with open(file_path, "w") as outfile:
         json.dump(geojson, outfile)
+
+    return file_path
+
+if __name__ == "__main__":
+    db = Database()
+
+    print("-" * 80)
+    print("Importing data from SLD/Map Service")
+    print("-" * 80)
+
+            
+    filepath = download_epa_sld_data_as_geojson("./SLD.geojson")
+    gdf = gpd.read_file(filepath)
+    explodedgdf = gdf.explode()
+    db.import_geodataframe(explodedgdf, new_tablename="sld", schema="extract")
+
+
